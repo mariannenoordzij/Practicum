9478184d3340fc1114d6d02142abca7787e54f1f
Marianne <marianne_noordzij@hotmail.com>
2018-11-22 14:04:50 +0100

    Finished

--- 01_FittsLaw/FittsLawShiyi2/FittsLawShiyi2/Program.cs
+++ 01_FittsLaw/FittsLawShiyi2/FittsLawShiyi2/Program.cs
@@ -18,7 +18,6 @@ namespace FittsLaw
         double distance;
         double reactie_ms;
         double ID;
-        int width;
 
         public InvoerForm()
         {
@@ -53,6 +52,35 @@ namespace FittsLaw
             measureButton.Click += MeasureButton_Click;
         }
 
+
+        void StartButton_Click(object sender, EventArgs e)
+        {
+            DateTime startTime = DateTime.Now;
+            this.st = startTime.Ticks;
+
+            Point startButtonLoc = startButton.FindForm().PointToClient(
+            startButton.Parent.PointToScreen(startButton.Location));
+
+            // random size measureButton
+            Random rndSize = new Random();
+            int rndMaxSize = rndSize.Next(25, 200);
+            Size sz = new Size(
+                    int.Parse(rndMaxSize.ToString()),
+                    int.Parse(rndMaxSize.ToString()));
+            measureButton.Size = sz;
+
+            // random positie 
+            Random rndPos = new Random();
+            Point pt = new Point(
+            int.Parse(rndPos.Next(ClientSize.Width - measureButton.Width).ToString()),
+            int.Parse(rndPos.Next(ClientSize.Height - measureButton.Width).ToString())
+            );
+            measureButton.Location = pt;
+
+            startButton.Hide();
+            measureButton.Show();
+        }
+
         void MeasureButton_Click(object sender, EventArgs e)
         {
             startButton.Show();
@@ -62,30 +90,11 @@ namespace FittsLaw
             DateTime measureTime = DateTime.Now;
             this.mt = measureTime.Ticks;
 
-            // random size measureButton
-            Random rndSize = new Random();
-            measureButton.MinimumSize = new Size(25, 25);
-            int MaxSize = 200;
-            int rndMaxSize = rndSize.Next(MaxSize);
-            Size sz = new Size(
-                    int.Parse(rndMaxSize.ToString()),
-                    int.Parse(rndMaxSize.ToString()));
-            measureButton.Size = sz;
-            this.width = measureButton.Width;
-
-            // random positie 
-            Random rndPos = new Random();
-            Point pt = new Point(
-            int.Parse(rndPos.Next(ClientSize.Width - width).ToString()),
-            int.Parse(rndPos.Next(ClientSize.Height - width).ToString())
-            );
-            measureButton.Location = pt;
-
             //// Krijg coordinaten van het centrum van measureButton en startButton
-            int mb_xpoint = measureButton.Location.X + (width / 2);
-            int mb_ypoint = measureButton.Location.Y - (width / 2);
-            int sb_xpoint = startButton.Location.X + (width / 2);
-            int sb_ypoint = startButton.Location.Y - (width / 2);
+            int mb_xpoint = measureButton.Location.X + (measureButton.Width / 2);
+            int mb_ypoint = measureButton.Location.Y + (measureButton.Width / 2);
+            int sb_xpoint = startButton.Location.X + (startButton.Width / 2);
+            int sb_ypoint = startButton.Location.Y + (startButton.Height / 2);
             int z1 = mb_xpoint - sb_xpoint;
             int z2 = mb_ypoint - sb_ypoint;
 
@@ -95,43 +104,15 @@ namespace FittsLaw
             // Reactietijd in milliseconden
             double reactie = mt - st;
             this.reactie_ms = reactie / 10000;
-            double dw = distance / width + 1;
+            double dw = distance / measureButton.Width + 1;
             double grondgetal = 2;
             this.ID = Math.Log(dw, grondgetal);
 
-            Console.WriteLine("Locatie startknop:" + "X: " + sb_xpoint + " Y: " + sb_ypoint);
-            Console.WriteLine("Measuretijd: " + mt);
-            Console.WriteLine("Width: " +  width);
-            Console.WriteLine("Helft:" + width / 2);
-            Console.WriteLine("X: " + mb_xpoint + ", Y: " + mb_ypoint);
+            Console.WriteLine("Width: " +  measureButton.Width);
             Console.WriteLine("Afstand = " + distance);
-            Console.WriteLine("Reactietijd = " + reactie);
-            Console.WriteLine("Reactietijd in ms = " + reactie_ms);
+            Console.WriteLine("T = " + reactie_ms);
             Console.WriteLine("ID = " + ID);
             Console.WriteLine("");
-        }
-        
-        void StartButton_Click(object sender, EventArgs e)
-        {
-            DateTime startTime = DateTime.Now;
-            this.st = startTime.Ticks;
-
-            Point startButtonLoc = startButton.FindForm().PointToClient(
-            startButton.Parent.PointToScreen(startButton.Location));
-
-            double x_sb = startButton.Location.X;
-            double y_sb = startButton.Location.Y;
-            double w_sb = startButton.Width / 2;
-            double h_sb = startButton.Width / 2;
-            double xpos = x_sb + w_sb;
-            double ypos = y_sb - h_sb;
-
-            // WriteLines
-            Console.WriteLine("Starttijd: " + st);
-            Console.WriteLine("Locatie startButton: " + x_sb.ToString() + ", " + y_sb.ToString());
-            Console.WriteLine("Centrum startButton: " + xpos.ToString() + ", " + ypos.ToString());
-            startButton.Hide();
-            measureButton.Show();
         }
     }
 
