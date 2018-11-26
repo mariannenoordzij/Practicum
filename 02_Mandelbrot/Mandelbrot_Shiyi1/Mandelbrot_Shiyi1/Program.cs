393be6400b66b30585d3f93048ce63e8fdacaccc
shiyibutter <shiyibutter@gmail.com>
2018-11-26 20:57:22 +0100

    Opzetje Mandelbrot, voornamelijk het design

---
+++ 02_Mandelbrot/Mandelbrot_Shiyi1/Mandelbrot_Shiyi1/Program.cs
@@ -0,0 +1,121 @@
+﻿/* Practicumopgave 2 Modelleren en Programmeren KI
+ * Mandelbrot
+ * Marianne Noordzij en Shi Yi Butter
+ */
+
+using System.Windows.Forms;
+using System.Drawing;
+using System;
+
+namespace Mandelbrot
+{
+    class InvoerForm : Form
+    {
+        Button button;
+
+        public InvoerForm()
+        {
+            // Layout window
+            this.Text = "Mandelbrot";
+            this.BackColor = Color.Gainsboro;
+            this.Size = new Size(525, 650);
+
+            // Laytout labelX;
+            Label labelX = new Label();
+            labelX.AutoSize = true;
+            labelX.Text = "midden X:";
+            labelX.Location = new Point(20, 15);
+
+            // Laytout labelY
+            Label labelY = new Label();
+            labelY.AutoSize = true;
+            labelY.Text = "midden Y:";
+            labelY.Location = new Point(20, 40);
+
+            // Laytout labelSchaal;
+            Label labelSchaal = new Label();
+            labelSchaal.AutoSize = true;
+            labelSchaal.Text = "schaal:";
+            labelSchaal.Location = new Point(200, 15);
+
+            // Laytout labelMax
+            Label labelMax = new Label();
+            labelMax.AutoSize = true;
+            labelMax.Text = "max:";
+            labelMax.Location = new Point(200, 40);
+
+            // Layout textbox midden X
+            TextBox middenX = new TextBox();
+            middenX.Name = "textboxX";
+            middenX.AutoSize = true;
+            middenX.Location = new Point(80, 13);
+
+            // Layout textbox midden Y
+            TextBox middenY = new TextBox();
+            middenY.Name = "textboxY";
+            middenY.AutoSize = true;
+            middenY.Location = new Point(80, 38);
+
+            // Layout textbox schaal
+            TextBox txtSchaal = new TextBox();
+            txtSchaal.Name = "textboxX";
+            txtSchaal.AutoSize = true;
+            txtSchaal.Location = new Point(248, 13);
+
+            // Layout textbox max
+            TextBox txtMax = new TextBox();
+            txtMax.Name = "textboxY";
+            txtMax.Size = new Size(45, 25);
+            txtMax.Location = new Point(248, 38);
+
+            // Layout button OK
+            Button button = new Button();
+            button.Name = "OK";
+            button.Text = "OK";
+            button.Size = new Size(45, 25);
+            button.Location = new Point(304, 38);
+
+            // Layout listbox
+            ListBox listbox = new ListBox();
+            listbox.Name = "listbox";
+            //listbox.ItemHeight = 40;
+            listbox.Size = new Size(60, 60);
+            listbox.Location = new Point(370, 13);
+            listbox.Items.Add("basis");
+            listbox.Items.Add("zuilen");
+            listbox.Items.Add("vuur");
+            listbox.Items.Add("zigzag");
+
+            this.Controls.Add(labelX);
+            this.Controls.Add(labelY);
+            this.Controls.Add(labelSchaal);
+            this.Controls.Add(labelMax);
+            this.Controls.Add(middenX);
+            this.Controls.Add(middenY);
+            this.Controls.Add(txtSchaal);
+            this.Controls.Add(txtMax);
+            this.Controls.Add(button);
+            this.Controls.Add(listbox);
+
+            button.Click += Button_Click;
+        }
+
+        void Button_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+
+    class Mandelbrot
+    {
+        static void Main()
+        {
+            InvoerForm scherm;
+            scherm = new InvoerForm();
+            Application.Run(scherm);
+        }
+        
+    }
+}
