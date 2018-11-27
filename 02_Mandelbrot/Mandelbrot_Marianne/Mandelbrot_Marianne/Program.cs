775da002ca0a7341016f4c152fbfcf7bd78a270d
Marianne <marianne_noordzij@hotmail.com>
2018-11-27 15:27:43 +0100

    mijn basic pagina

---
+++ 02_Mandelbrot/Mandelbrot_Marianne/Mandelbrot_Marianne/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Windows.Forms;
+using System.Drawing;
+using System;
+
+namespace Mandelbrot
+{
+    class GUI : Form
+    {
+       
+        public GUI()
+        {
+            this.Text = "Mandelbrot Figure";
+            this.BackColor = Color.White;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Size = new Size(Screen.PrimaryScreen.WorkingArea.Width, Screen.PrimaryScreen.WorkingArea.Height);
+
+            Label label_co_X = new Label();
+            Label label_co_Y = new Label();
+            Label label_schaal = new Label();
+            Label label_max = new Label();
+
+            label_co_X.Location = new Point(20, 20);
+            label_co_X.Size = new Size(50, 20);
+            label_co_X.Text = "X-coord";
+
+            label_co_Y.Location = new Point(20, 60);
+            label_co_Y.Size = new Size(50, 20);
+            label_co_Y.Text = "Y-coord";
+
+            Controls.Add(label_co_X);
+            Controls.Add(label_co_Y);
+        }
+    }
+
+    class Mandelbrot
+    {
+        static void Main()
+        {
+            GUI scherm;
+            scherm = new GUI();
+            Application.Run(scherm);
+        }
+    }
+}
+
